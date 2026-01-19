feba7141db44aae998b7b8d95c4218a142375000
atharvyadav22 <dev.atharvyadav@gmail.com>
2026-01-19 10:45:08 +0530

    made changes to support render ie added lazy initialization in JwtAuthFilter

--- src/main/java/org/aystudios/Skincare/util/JwtAuthFilter.java
+++ src/main/java/org/aystudios/Skincare/util/JwtAuthFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.aystudios.Skincare.entity.UserEntity;
 import org.aystudios.Skincare.repository.UserRepository;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +22,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
 
-    public JwtAuthFilter(JwtUtil jwtUtil, UserRepository userRepository) {
+    public JwtAuthFilter(JwtUtil jwtUtil, @Lazy UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
         this.userRepository = userRepository;
     }
